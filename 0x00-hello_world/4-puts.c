a08e20af0c66a708f04db428fb9d4a773e119826
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-05 03:13:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ *  main - prints a string followed by a new line
+ *  Return: 0 return value
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingul puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
