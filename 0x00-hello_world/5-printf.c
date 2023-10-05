536d445a8e1820453f5eaf10dc1db35a5ecc35fd
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-05 03:23:29 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- print a string folowed by a new line
+ * Return: 0- exit status
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
