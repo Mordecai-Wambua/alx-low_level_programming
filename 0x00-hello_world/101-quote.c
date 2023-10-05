22f2a8e5c6665259e48f728074adc113d5827aa3
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-05 10:40:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
-/** 
- * main- prints a string
+/**
+ * main- print out a string without printf or puts function
  *
- * Return: 1- error
+ * Return: 1-error
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
+	char *str = "and that pieceof art is useful\" -Dora Korpar, 2015-10-19\n";
+
+	while (*str)
+		write(STDERR_FILENO, str++, 1);
+
 	return (1);
 }
