0966e297600a5e5dbdad869abb51097bd3159666
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-06 08:25:24 -0700

    task 000

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{printf("%d is positive number\n"); }
+	{printf("%d is positive number\n", n); }
 	else if (n < 0)
-	{printf("%d is negative number\n"); }
+	{printf("%i is negative number\n", n); }
 	else
-	{printf("%d is zero\n"); }
+	{printf("%i is zero\n", n); }
 
 	return (0);
 }
