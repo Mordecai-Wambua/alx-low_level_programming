5c6c0ccc18dae2f5b0ccf43325e971013c23021d
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 09:19:30 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day from 00:00 to 23:59
+ *
+ * Return: 0- success
+ *
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+			_putchar(':');
+			_putchar('0' + b / 10);
+			_putchar('0' + b % 10);
+			_putchar('\n');
+		}
+	}
+}
