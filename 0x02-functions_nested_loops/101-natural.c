978eaf4154d8d46099c62c5a672fe6488a2e1821
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 12:16:24 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- enrty
+ *
+ * Return: 0-success
+ */
+int main(void)
+{
+	int x, y = 0, 0;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			y += x;
+		}
+		x++;
+	}
+	printf("%i\n", y);
+	return (0);
+}
