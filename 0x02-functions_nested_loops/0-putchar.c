00673b57665bdb48c82bd981629dbf7fd48f10c8
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 07:09:30 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main-entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: 0- successful execution
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
