efba20fb3c67e530ce5e5edb86b390e0cf534ba7
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 12:32:24 -0700

    task 13

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int x = 50;
 	int i;
-	unsigned long long a = 1, b = 2, next;
+	long long a = 1, b = 2, next;
 
-	printf("%llu, %llu, ", a, b);
+	printf("%lld, %lld, ", a, b);
 
-	for (i = 3; i <= n; i++)
+	for (i = 3; i <= x; i++)
 	{
 		next = a + b;
-		printf("%llu", next);
+		printf("%lld", next);
 
 		if (i < x)
 		{
