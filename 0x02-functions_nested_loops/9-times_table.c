dc3488d481ab7a0f5dce0682c551315030f0b674
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 09:37:13 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table
+ *
+ * Return: 0-success
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+
+			if (y == 0)
+			{
+				_putchar('0');
+			}
+			else if (z < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + z);
+			}
+			else
+			{
+				_putchar('0' + z / 10);
+				_putchar('0' + z % 10);
+			}
+		}
+		if (y < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
+
