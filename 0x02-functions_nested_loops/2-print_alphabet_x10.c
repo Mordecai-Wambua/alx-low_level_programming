d4fa86845958e45f1bf75e5c409868935ef56e13
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 07:38:11 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: print the alphabet in lowercase 10 times
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
