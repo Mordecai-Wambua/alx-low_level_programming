f9090848645e3bf7c2ee3682e66fc5d3392c1394
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 10:32:52 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
