9529a28c7b8b6bf46e7e30196851eff641167555
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-11 10:44:11 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: entered value
+ * Return: 0-success
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n > 98)
+		{
+			n--;
+		}
+		else
+		{
+			n++;
+		}
+	}
+	printf("98\n");
+}
