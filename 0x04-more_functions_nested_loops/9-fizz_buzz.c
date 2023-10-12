dcf40f48ec0582596e20c869d321ac79b12b434c
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-12 04:40:29 -0700

    tasks

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 1; x < 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz ");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else if (x % 3 == 0 && x % 5 == 0)
+		else if (x % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz ");
 		}
 		else
 		{
