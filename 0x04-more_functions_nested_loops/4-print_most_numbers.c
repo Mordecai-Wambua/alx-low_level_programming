3eb452f3ed0b9d383baa9b392a9f5f7ce33113d1
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-12 03:50:32 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,10 @@ void print_most_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if (x != 2 || x != 4)
+		if (x != 2 && x != 4)
 		{
 			_putchar('0' + x);
 		}
 	}
 	_putchar('\n');
-	return (0);
 }
