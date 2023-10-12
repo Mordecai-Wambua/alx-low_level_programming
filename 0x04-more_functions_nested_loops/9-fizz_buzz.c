28e18aa41cfb729705c09530d1b7d5fc1be0f9fe
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-12 04:52:06 -0700

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int x, y;
+	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
@@ -27,6 +27,7 @@ int main(void)
 		{
 			printf("%d ", x);
 		}
+		if (x
 	}
 	printf("\n");
 	return (0);
