1fac385e115dff597f6859c96dc3eec300ec8318
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-13 15:07:16 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,38 +13,29 @@
 void print_remaining_days(int month, int day, int year)
 {
 	int m_days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int i, y_day, z;
 
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
 		m_days[2] = 29;
-		int y_day = day;
-
+		y_day = day;
+		
 		if (month < 1 || month > 12 || day < 1 || day > m_days[month])
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		}
-		for (int i = 1; i < month; i++)
-		{
-			y_day += m_days[i];
-		}
-		printf("Day of the year: %d\n", y_day);
-		printf("Remaining days: %d\n", 366 - y_day);
+                {
+                        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+                }
 	}
 	else
-	{
-		if (month < 1 || month > 12 || day < 1 || day > m_days[month])
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		}
+	{		
+		y_day = day;
 
-		int y_day = day;
-
-		for (int i = 1; i < month; i++)
+		for (i = 1; i < month; i++)
 		{
 			y_day += m_days[i];
 		}
 
+		z = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 366 : 365;
 		printf("Day of the year: %d\n", y_day);
-		printf("Remaining days: %d\n", 365 - y_day);
+		printf("Remaining days: %d\n", z - y_day);
 	}
 }
