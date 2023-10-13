1d3f51f473be2593dad5f35622eff08f09811917
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-13 03:45:26 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * main - test function for output 0
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int a = 0;
+        int i;
 
-	postive_or_negative(a);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
