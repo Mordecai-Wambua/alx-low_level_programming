7bbdd165930754ba61267f0abc88eec95db63d34
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-16 09:48:07 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ * reset_to_98 - update value to 98
+ * @n: initial value
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+task 00:
+function to take a pointer to an int as parameter and update the value it points to to 98
