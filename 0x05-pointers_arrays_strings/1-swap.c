e8a0c8ef35bc5d0bd9652c6014f23b6f1dce4f85
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-16 10:36:32 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of 2 integers
+ * @a: memory address of a
+ * @b: memory address of b
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
