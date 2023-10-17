a358646d0f0dbbef3d9f21148acda066073be348
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-17 13:30:47 +0300

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int l, i, x;
+	
+	l = strlen(str);
+	if (l % 2 != 0)
+	{
+		x = (l - 1) / 2;
+	}
+	else
+	{
+		x = l / 2;
+	}
+	for (i = x; i < l; i++)
+	{
+		printf("%c", str[i];
+	}
+}
