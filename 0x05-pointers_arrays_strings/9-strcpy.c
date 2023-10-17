d3c4ce960dc3a9aa6f36ee4216af66a34ac80f56
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-17 17:42:59 +0300

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,14 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (i >= 0)
 	{
-		*(dest + 1) = src[i];
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+		{
+			break;
+		}
 		i++;
 	}
-	*(dest + i) = '\0';
 	return (dest);
 }
