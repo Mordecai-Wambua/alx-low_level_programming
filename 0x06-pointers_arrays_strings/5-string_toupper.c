cf7ce3c741afcafc8b69832a0ffcc8af6b1cbb1a
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-19 20:25:15 +0300

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,3 +8,14 @@
 
 char *string_toupper(char *str)
 {
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+	}
+	return (str);
+}
