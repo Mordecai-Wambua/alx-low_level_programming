8e7bdc28e6acbc1795c07a368de166d11c692f05
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-23 16:16:18 +0300

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (&s[i]);
+			return (s);
 		}
+		s++;
 	}
 	return (NULL);
 }
