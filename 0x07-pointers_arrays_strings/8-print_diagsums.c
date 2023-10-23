333d508701d9ef6e91349e38e0bc47a3d54d4c1c
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-23 20:32:19 +0300

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,5 +18,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - i - 1)];
 	}
-	printf("%d, %d \n", sum1, sum2);
+	printf("%i, %i \n", sum1, sum2);
 }
