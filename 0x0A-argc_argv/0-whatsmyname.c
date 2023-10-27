32c0e2fe7da78d3773110d12a79dadb56c29b12d
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-10-27 16:12:22 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+Task 00: (0-whatsmyname.c)
+program that prints iits name followed by a new line
