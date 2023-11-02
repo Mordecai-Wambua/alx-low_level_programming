3904b09eefddab7b0bc162c0dcf53104d2c58fcc
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-02 13:48:29 +0300

    task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: bytes per element
+ *
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		array[i] = 0;
+	}
+
+	return (array);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -3,3 +3,6 @@ function that allocates memory using malloc
 
 Task 01:(1-string_nconcat.c)
 function that concatenates two strings
+
+Task 02:(2-calloc.c)
+function that allocates memory for an array using malloc
