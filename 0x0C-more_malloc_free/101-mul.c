f55a4fc9aa8d9c05e842f3d1c222a36c5c659822
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-02 17:32:36 +0300

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,58 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
-int validity(char *);
 
 /**
- * main - multiplies two positive numbers
+ * is_number - checks validity of a number
+ * @str: input number
+ *
+ * Return: 1 = valid, 0 = invalid
+ */
+
+int is_number(char *str)
+{
+	int i = 0;
+
+	if (str == NULL || str[0] == '\0')
+		return (0);
+	if (str[0] == '+' || str[0] == '-')
+		i++;
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * mul - multiplies the two numbers
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: char
+ */
+
+char *mul(char *num1, char *num2)
+{
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+	int len = len1 + len2;
+	char *result = malloc(len + 1);
+	int i, j, k, carry, prod;
+
+	for (i = 0; i < len; i++)
+		result[i] = '0';
+	result[len] = '\0';
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		k = len - (len1 - i);
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			prod = (num1[i] - '0') * (num2[j] - '0') + carry + (result[k] - '0');
+			result[k] = (prod % 10) + '0';
+			carry = prod / 10;
+			k--;
+		}
+		while (carry > 0)
+		{
+			prod = carry + (result[k] - '0');
+			result[k] = (prod % 10) + '0';
+			carry = prod / 10;
+			k--;
+		}
+	}
+	i = 0;
+	while (result[i] == '0' && i < len - 1)
+		i++;
+	return (result + i);
+}
+
+
+/**
+ * main - entry point
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 = success, 98 = fail
+ * Return: int
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int mul, num0, num1;
+	char *num1, *num2, *result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (!validity(argv[1]) || !validity(argv[2]))
+	num1 = argv[1];
+	num2 = argv[2];
+	if (!is_number(num1) || !is_number(num2))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num0 = atoi(argv[1]);
-	num1 = atoi(argv[2]);
-
-	mul = num0 * num1;
-	printf("%d\n", mul);
-
+	result = mul(num1, num2);
+	printf("%s\n", result);
 	return (0);
 }
 
-/**
- * validity - checks if inputs are numbers
- * @str: input
- *
- * Return: 1 = valid, 0 = invalid
- */
-
-int validity(char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
