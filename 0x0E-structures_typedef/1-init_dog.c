01a2dc449fc2ed09bd06191a2c76e0ef637b1591
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-06 15:39:08 +0300

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,2 +1,5 @@
 Task 00:(dog.h)
 defines a new type struct dog
+
+Task 01:(1-init_dog.c)
+function that initializes a variable of tyoe struct dog
