b60b9ae95ee0b504cc81f5072331b158dfeec3e0
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-06 14:09:15 +0300

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: zero
+ */
+
 int main(void)
 {
 
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
