96e0bb1e0cdd4cfc9a78a77e6efdb3f59fc86266
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-06 17:08:09 +0300

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ *
+ * Return: dog or NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *name_copy;
+	char *owner_copy;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(dog);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
+
+	dog->name = name_copy;
+	dog->age = age;
+	dog->owner = owner_copy;
+
+	return (dog);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -9,3 +9,9 @@ function that prints a struct dog
 
 Task 03:(dog.h)
 defines a new type dog_t as a new name of the type struct dog
+
+Task 04:(4-new_dog.c)
+function that creates a new dog
+
+Task 05:(5-free_dog.c)
+function that frees dogs
