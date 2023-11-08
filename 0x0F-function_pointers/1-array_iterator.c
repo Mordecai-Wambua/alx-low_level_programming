1a639836777283a8c91b28eee6b71d558d479d4f
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-08 12:47:51 +0300

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
