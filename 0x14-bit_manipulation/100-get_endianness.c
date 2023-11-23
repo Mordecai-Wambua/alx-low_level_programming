dc82d944999c96e2c7f84c0f28b9fb5e6b2e793c
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-23 14:58:32 +0300

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * @void:
+ *
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
