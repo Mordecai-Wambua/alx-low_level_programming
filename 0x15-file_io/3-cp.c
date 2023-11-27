14c602adea2669ec8a27d275aa50180688eeedd6
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-27 22:05:04 +0300

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int source, final, r;
 	char buffer[BUFFER_SIZE];
-	
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
