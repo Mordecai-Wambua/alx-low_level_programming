b587c2294f246b1fe5bc22683c2c10d07a36d22b
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-11-27 22:16:17 +0300

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC | 0600);
 	if (f == -1)
 	{
 		return (-1);
@@ -23,9 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		w = write(f, text_content, strlen(text_content));
-		if (w == -1 || (size_t)w != strlen(text_content))
+		if (w == -1)
 		{
-			close(f);
 			return (-1);
 		}
 	}
