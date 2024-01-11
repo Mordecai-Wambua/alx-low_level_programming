21db4c04634f945101d8e2a600343755b85cbdba
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-11 18:35:18 +0300

    task 00

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,18 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x = 0;
 
-	if (h == NULL)
+	while (h != NULL)
 	{
-		return (x);
-	}
-	else
-	{
-		while (h != NULL)
-		{
-			printf("%u\n", h->n);
-			h = h->next;
-			x++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
 	}
 	return (x);
 }
