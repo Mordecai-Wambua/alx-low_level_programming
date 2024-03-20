976f1a60a501119b7215aa508bf0dfd0c98bd0c0
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-03-20 22:47:23 +0300

    task 05

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Calculates the perimeter based on a list of integers passed."""
+
 
 def island_perimeter(grid):
+    """Calculate perimeter of the island.
+
+    Args:
+        grid(list): list of list of integers.
+    """
     w = len(grid[0])
     h = len(grid)
     p = 0
